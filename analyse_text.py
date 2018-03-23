9740f82acd12eb0dd1f591e9998c2bbed588c37c
Ben North <ben@redfrontdoor.org>
2018-03-23 08:33:29 +0000

    augment_summary(): Add

--- analyse_text.py
+++ analyse_text.py
@@ -6,3 +6,9 @@ def get_summary(text):
     if len(text) == 0:
         raise TextTooShortError
     return {'head': text[0]}
+
+
+def augment_summary(text, summary):
+    aug_summary = dict(summary)
+    aug_summary['n_characters'] = len(text)
+    return aug_summary
