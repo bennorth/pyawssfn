d4db45c5fc294e807175d6a003826149d6fbe47f
Ben North <ben@redfrontdoor.org>
2018-03-23 08:36:42 +0000

    get_n_vowels(): Test

--- test_analyse_text.py
+++ test_analyse_text.py
@@ -14,3 +14,7 @@ class TestAnalysis:
         aug_summary = A.augment_summary('hello world', summary)
         assert aug_summary['head'] == 'h'  # Original data should remain
         assert aug_summary['n_characters'] == 11
+
+    def test_get_n_vowels(self):
+        assert A.get_n_vowels('hello world') == 3
+        assert A.get_n_vowels('rhythms') == 0
