20817e8eb2eec60f173837ef1accad5a7e5e2b7b
Ben North <ben@redfrontdoor.org>
2018-03-23 08:30:52 +0000

    get_summary(): Test behaviour on empty input

--- test_analyse_text.py
+++ test_analyse_text.py
@@ -4,3 +4,7 @@ import analyse_text as A
 class TestAnalysis:
     def test_get_summary(self):
         assert A.get_summary('hello world') == {'head': 'h'}
+
+    def test_get_summary_too_short(self):
+        with pytest.raises(A.TextTooShortError):
+            A.get_summary('')
