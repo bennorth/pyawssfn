2cf9b2083a9cc591786c9149d9db31ba98208c15
Ben North <ben@redfrontdoor.org>
2018-03-23 23:09:18 +0000

    psf_attr(): Test error cases

--- test_pysfnc.py
+++ test_pysfnc.py
@@ -1,3 +1,4 @@
+import pytest
 import pysfnc as C
 import ast
 
@@ -10,3 +11,15 @@ class TestSupportFunctions:
     def test_psf_attr(self):
         val = expr_value('PSF.hello_world')
         assert C.psf_attr(val) == 'hello_world'
+
+    @pytest.mark.parametrize(
+        'text',
+        ['99 + 42',
+         'something_else.odd',
+         'PSF.nested.attribute']
+    )
+    def test_psf_attr_bad_input(self, text):
+        val = expr_value(text)
+        with pytest.raises(ValueError):
+            C.psf_attr(val)
+        assert C.psf_attr(val, raise_if_not=False) is None
