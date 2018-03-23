a0fa0fe3ea01832379dae5f45fd79ad7c3c241c0
Ben North <ben@redfrontdoor.org>
2018-03-23 23:19:37 +0000

    chained_key(): Add helper function

--- pysfnc.py
+++ pysfnc.py
@@ -21,3 +21,29 @@ def psf_attr(nd, raise_if_not=True):
     if attr_val is None and raise_if_not:
         raise ValueError('expected PSF.something')
     return attr_val
+
+
+def chained_key(nd):
+    """
+    Given an AST node representing a value like
+
+        foo['bar']['baz']
+
+    return a list of the components involved; here,
+
+        ['foo', 'bar', 'baz']
+
+    If the given node is not of that form, raise a ValueError.
+    """
+    if isinstance(nd, ast.Name):
+        return [nd.id]
+    if isinstance(nd, ast.Subscript):
+        if isinstance(nd.slice, ast.Index):
+            if isinstance(nd.slice.value, ast.Str):
+                suffix = nd.slice.value.s
+                if isinstance(nd.value, ast.Name):
+                    prefix = [nd.value.id]
+                else:
+                    prefix = chained_key(nd.value)
+                return prefix + [suffix]
+    raise ValueError('expected chained lookup via strings on name')
