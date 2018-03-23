06295eaaa0fd6124a1e2f7e0c3bfa0a365982314
Ben North <ben@redfrontdoor.org>
2018-03-23 23:09:03 +0000

    psf_attr(): Test normal case

--- test_pysfnc.py
+++ test_pysfnc.py
@@ -1,5 +1,12 @@
+import pysfnc as C
 import ast
 
 
 def expr_value(txt):
     return ast.parse(txt).body[0].value
+
+
+class TestSupportFunctions:
+    def test_psf_attr(self):
+        val = expr_value('PSF.hello_world')
+        assert C.psf_attr(val) == 'hello_world'
